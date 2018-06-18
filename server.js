d0886742e2a32208943fc70a76b5258937aa1e5e
Praveen Venigalla <praveenv.aiesec@gmail.com>
2018-06-18 19:56:54 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+let app = express();
+
+app.set('view engine','hbs');
+
+hbs.registerPartials(__dirname+'/views/partials');
+
+hbs.registerHelper('getCurrentYear',()=>{
+    return new Date().getFullYear();
+});
+
+console.log(__dirname);
+
+
+
+app.use((req,res,next)=>{
+
+    var now = new Date().toString();
+    var log = `${log} : ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log',log+'\n',(err)=>{
+        if(err){
+            console.log("unable to copy");
+        }
+    });
+
+    next();
+
+});
+
+
+// app.use((req,res,next)=>{
+//
+//
+//     res.render('maintainance.hbs');
+//
+//
+// });
+
+app.use(express.static(__dirname+'/public'));
+
+app.get('/',(req,res)=> {
+
+    res.render('home.hbs',{
+        pageTitle : 'home page',
+        welcomeMessage : 'welcome my home page'
+    });
+
+});
+
+
+
+
+app.get('/about',(req,res)=> {
+    //res.send('About page');
+
+    res.render('about.hbs',{
+        pageTitle : 'about page'
+    });
+});
+
+app.listen(3000),()=>{
+    console.log('server is up on port 3000');
+};
